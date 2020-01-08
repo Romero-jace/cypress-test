1ef07d724c481742b7925c24ca3b7f900b975588
Jace A. Romero <romero.jace@gmail.com>
2020-01-08 17:51:02 -0600

    Initial Commit

---
+++ Cypress/cypress/integration/page_objects/object_file.js
@@ -0,0 +1,72 @@
+export class pageObjects {
+    home() {
+        return cy.visit('https://www.volusion.com/')
+    }
+
+    logo() {
+        return cy.get("")
+    }
+    featuresNavBar() {
+        return cy.get("adata-test=boop")
+    }
+
+    pricingNavBar() {
+        return 
+    }
+
+    themeseNavBar() {
+
+    }
+
+    customersNavBar() {
+
+    }
+
+    resourcesNavBar() {
+
+    }
+
+    studioNavBar() {
+
+    }
+
+    phoneNavBar() {
+
+    }
+
+    loginNavBar() {
+
+    }
+
+    callButton() {
+
+    }
+
+    chatButton() {
+
+    }
+
+    pricingPlansButton() {
+
+    }
+
+    createLearnMoreButton() {
+
+    }
+
+    sellLearnMoreButton() {
+
+    }
+
+    growLearnMoreButton() {
+
+    }
+
+    helpLearnMoreButton() {
+
+    }
+
+    startSellingButton() {
+
+    }
+}
